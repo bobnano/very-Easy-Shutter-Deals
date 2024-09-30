5a86d894aa365839150fbb34fc144537fefbe5f9
bobnano <142730046+bobnano@users.noreply.github.com>
2024-09-30 22:31:29 +0530

    Update products.js

--- src/products.js
+++ src/products.js
@@ -8,14 +8,14 @@ const products = [
       link:"https://amzn.to/3BneSUH",
       img:"https://m.media-amazon.com/images/I/618VuqprX7L._SL1500_.jpg"
     },
-    // {
-    //     id: 4,
-    //     title: "Spigen 5in1 10000mAh Wireless Power Bank",
-    //     category: "Mobile Mccessories",
-    //     description: "This 5-in-1 wireless charging station offers a convenient solution for charging multiple devices simultaneously. It features a 10,000mAh power bank, fast charging capabilities (USB-C 20W, USB-A 18W, wireless charging up to 15W), and compatibility with a wide range of smartphones, earbuds, and smartwatches. With its built-in mobile phone stand, you can easily charge and use your devices hands-free.",
-    //     link:"",
-    //     img:"https://m.media-amazon.com/images/I/618VuqprX7L._SL1500_.jpg"
-    //   },
+    {
+        id: 4,
+        title: "Realme Buds T310 Truly Wireless in-Ear Earbuds",
+        category: "Mobile Mccessories",
+        description: "This Tws comes With 46dB Hybrid ANC, 360° Spatial Audio, 12.4mm Dynamic Bass Driver, Upto 40Hrs Battery and Fast Charging (Monet Purple)",
+        link:"https://amzn.to/3N8TXag",
+        img:"https://m.media-amazon.com/images/I/61grOKkDUvL._SL1500_.jpg"
+    },
     //   {
     //     id: 3,
     //     title: "Spigen 5in1 10000mAh Wireless Power Bank",
