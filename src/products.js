51dc803625e442582204f803efc4245675829f53
bobnano <142730046+bobnano@users.noreply.github.com>
2024-09-30 22:15:25 +0530

    Update products.js

--- src/products.js
+++ src/products.js
@@ -5,7 +5,7 @@ const products = [
       title: "Spigen 5in1 10000mAh Wireless Power Bank",
       category: "Mobile Accessories",
       description: "This 5-in-1 wireless charging station offers a convenient solution for charging multiple devices simultaneously. It features a 10,000mAh power bank, fast charging capabilities (USB-C 20W, USB-A 18W, wireless charging up to 15W), and compatibility with a wide range of smartphones, earbuds, and smartwatches. With its built-in mobile phone stand, you can easily charge and use your devices hands-free.",
-      link:"",
+      link:"https://amzn.to/3BneSUH",
       img:"https://m.media-amazon.com/images/I/618VuqprX7L._SL1500_.jpg"
     },
     // {
